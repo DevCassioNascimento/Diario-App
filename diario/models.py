600a36e3c982773a3d5aea8d90792f6d67ca797f
DevCassioNascimento <cassionascimentovieira@outlook.com>
2025-01-21 22:16:52 -0300

    adicionando pessoa

--- diario/models.py
+++ diario/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    nome = models.CharField(max_length=100)
+    fotos = models.ImageField(upload_to='fotos')
+
+    def __str__(self):
+        return self.nome
+    
+    class Diario(models.Model):
+        titulo = models.CharField(max_length=100)
+        tags = models.TextField()
+        pessoas = models.ManyToManyField('Pessoa')
+        texto = models.TextField()
+        created_at = models.DateTimeField(auto_now_add=True)
+        
+        def __str__(self):
+            return self.titulo
--- diario/views.py
+++ diario/views.py
@@ -1,7 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Pessoa, Diario
 
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def escrever(request):
+    if request.method == 'GET':
+       pessoa = Pessoa.objects.all()
+       return render(request, 'escrever.html', {'pessoa': pessoa})
+    elif request.method == 'POST':
+       titulo = request.POST.get('titulo')
+       tags = request.POST.getlist('tags')
+       pessoas = request.POST.getlist('pessoas')
+       texto = request.POST.get('texto')
+       
+       if len(titulo.strip()) == 0 or len(texto.strip()) == 0:
+           return HttpResponse('Por favor, preencha todos os campos')
+       
+       diario=Diario(
+           titulo=titulo,
+           texto=texto
+       )
+       diario.save()
+       
+       for i in pessoa:
+           pessoa = Pessoa.objects.get(id=i)
+           diario.pessoa.add(pessoa)
+           diario.save()
+       
+       return redirect('home')
+   
+def cadastrar_pessoa(request):
+    if request.method == 'GET':
+        return render(request, 'pessoa.html')
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        foto = request.FILES.get('foto')
+        
+        pessoa=Pessoa(
+            nome=nome,
+            fotos=foto
+        )
+        
+        pessoa.save()
+        return redirect('escrever')
